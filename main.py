fa3b953d90bbfc84206441153139a3dc35092d10
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 13:34:20 -0700

    While loop complete

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+  
     total = 0
+    i = 0
+    while (i < 5):
+        num = int(input('Enter a number: '))
+        total += num
+        i += 1
+    print(total)
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
     return total
 
 
